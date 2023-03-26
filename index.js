323d1a539dad8036fec5739cc0c5dd056e3cc247
pavan <pashampavan02@gmail.com>
2023-03-26 20:26:44 +0530

    parallax

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ let mountain=document.getElementById('mountain');
 let lightning=document.getElementById('lightning');
 window.addEventListener('scroll',function(){
     let value=window.scrollY;
-    lightning.style.marginTop=10+value*.7+'px';
-    lightning.style.marginLeft=1100+-value*.7+'px';
+    lightning.style.marginTop=60+value*.8+'px';
+    lightning.style.marginLeft=1100+-value*.8+'px';
     fullMoon.style.top=200+value*.3+'px';
     box.style.marginTop=value*.6+'px';
+    mountain.style.bottom=-88+value*.2+'px';
 })
